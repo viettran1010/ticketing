1b1e7872fd5b483184d95cf1f0fd14394113c112
viettran1010 <viettran10101988@gmail.com>
2021-02-23 19:52:10 +0700

    connect to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import 'express-async-errors'
 import {json} from 'body-parser'
+import mongoose from 'mongoose'
+
 import { currentuserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
@@ -16,13 +18,29 @@ app.use(currentuserRouter)
 app.use(signinRouter)
 app.use(signupRouter)
 app.use(signoutRouter)
+app.use(errorHandler)
+
 
 app.all('*', async (req,res)=> {
     throw new NotFoundError();
 })
 
-app.use(errorHandler)
+const start = async ()=> {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Connected to mongodb')
+    } 
+    catch (err) {
+        console.error(err);
+    }
 
-app.listen(3000, ()=> {
-    console.log('Listening on 3000')
-})
+    app.listen(3000, ()=> {
+        console.log('Listening on 3000')
+    })
+}
+
+start();
