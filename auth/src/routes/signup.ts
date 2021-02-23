dafd64732e0f1d9aa066bd0dbf42752ca92311d2
viettran1010 <viettran10101988@gmail.com>
2021-02-23 20:45:47 +0700

    create user logic

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,28 +1,35 @@
 import express, {Request, Response} from 'express'
 import {body, validationResult} from 'express-validator'
 import {RequestValidaionError} from '../errors/request-validation-errors'
-import {DatabaseConnectionError} from '../errors/database-connection-errors'
+import {User} from '../models/user'
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength( {min: 4, max: 20}).withMessage('Password must be 4-20 characters')
-] , (req: Request,res: Response)=> {
+] , async (req: Request,res: Response)=> {
 
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         throw new RequestValidaionError(errors.array());
     }
-
+    
     const {email, password} = req.body;
 
-    console.log('Creating user...');
+    const existingUser = await User.findOne({email}).exec();
 
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+        console.log('Email in use');
+        return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({email, password});
+
+    await user.save();
+
+    res.status(201).send(user);
 })
 
 export {router as signupRouter}
