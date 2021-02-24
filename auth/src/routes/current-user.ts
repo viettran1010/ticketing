48b172be56b395f85f9006befbd3ffee2fe1d27b
viettran1010 <viettran10101988@gmail.com>
2021-02-24 22:13:04 +0700

    get current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req,res)=> {
-    res.send('Hi there')
+    if (!req.session?.jwt) { // no jwt in cookies, "?" is TS's check for null
+        return res.send({currentuser: null})
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        res.send({currentuser: payload})
+    }
+    catch (err) {
+        return res.send({currentuser: null})
+    }
 })
 
 export {router as currentuserRouter}
