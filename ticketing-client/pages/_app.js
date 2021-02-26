660c67e50f9f3c7876131d36c4c6332c281253a8
viettran1010 <viettran10101988@gmail.com>
2021-02-26 20:23:39 +0700

    pass props

--- ticketing-client/pages/_app.js
+++ ticketing-client/pages/_app.js
@@ -1,5 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css'; // global css here
+import buildClient from '../api/build-client'
 
-export default ({Component, pageProps})=> {
-    return <Component {...pageProps} />
+const AppComponent = ({Component, pageProps, currentUser})=> {
+    return <div>
+        <h1>Header {currentUser.email}</h1>
+        <Component {...pageProps} />
+    </div>
 }
+
+AppComponent.getInitialProps = async (appContext)=> {
+    const {data} = await buildClient(appContext.ctx).get('/api/users/currentuser');
+
+    let pageProps = {};
+
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    return {
+        pageProps,
+        ...data
+    }
+}
+
+export default AppComponent
--- ticketing-client/pages/index.js
+++ ticketing-client/pages/index.js
@@ -1,12 +1,11 @@
 import buildClient from '../api/build-client'
 
 const LandingPage = ({currentUser})=> {
-    console.log(currentUser)
-    return <h1>Landing Page</h1>
+    return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in!</h1>
 }
 
-LandingPage.getInitialProps = async ({req})=> {
-    const {data} = await buildClient({req}).get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context)=> {
+    const {data} = await buildClient(context).get('/api/users/currentuser');
     return data
 }
 
