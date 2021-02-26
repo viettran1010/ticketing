cf34a4d49b4251afca14134cedd63264201dea8e
viettran1010 <viettran10101988@gmail.com>
2021-02-26 19:27:25 +0700

    custom baseURL for axios

--- ticketing-client/pages/index.js
+++ ticketing-client/pages/index.js
@@ -1,3 +1,13 @@
-export default ()=> {
+import buildClient from '../api/build-client'
+
+const LandingPage = ({currentUser})=> {
+    console.log(currentUser)
     return <h1>Landing Page</h1>
 }
+
+LandingPage.getInitialProps = async ({req})=> {
+    const {data} = await buildClient({req}).get('/api/users/currentuser');
+    return data
+}
+
+export default LandingPage;
