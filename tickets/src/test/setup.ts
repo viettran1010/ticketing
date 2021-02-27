4a22952e55bc603e473fc14a849828b270bc84a8
viettran1010 <viettran10101988@gmail.com>
2021-02-27 21:33:47 +0700

    update ticket tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -42,7 +42,7 @@ afterAll(async () => {
 global.signin = () => {
   // build a JWT payload {id, email}
   const payload = {
-    id: '21wfafawd',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com'
   }
 
